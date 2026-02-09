af7aba156e9745755dc21269f098260817c4f9d5
tk <tk@example.org>
2026-02-09 11:59:56 +0100

    move unmarshaller logic

--- speedtest-framework/src/main/java/org/example/framework/out/xml/Context.java
+++ speedtest-framework/src/main/java/org/example/framework/out/xml/Context.java
@@ -1,11 +1,13 @@
 package org.example.framework.out.xml;
 
+import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.xml.bind.JAXBContext;
 import jakarta.xml.bind.JAXBException;
 import jakarta.xml.bind.Unmarshaller;
 
 import java.io.InputStream;
 
+@ApplicationScoped
 public class Context {
 
     @SuppressWarnings("unchecked")
