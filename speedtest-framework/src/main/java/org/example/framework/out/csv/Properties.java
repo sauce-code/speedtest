c0fe45932b807176a8e976d68ee72fb10d344ac2
tk <tk@example.org>
2026-02-16 22:43:52 +0100

    improve csv export

--- speedtest-framework/src/main/java/org/example/framework/out/csv/Properties.java
+++ speedtest-framework/src/main/java/org/example/framework/out/csv/Properties.java
@@ -3,10 +3,17 @@ package org.example.framework.out.csv;
 import io.smallrye.config.ConfigMapping;
 
 import java.io.File;
+import java.nio.charset.Charset;
 
 @ConfigMapping(prefix = "speedtest.csv")
 public interface Properties {
 
     File file();
+
+    Charset charset();
+
+    String delimiter();
+
+    String rowSeparator();
 
 }
